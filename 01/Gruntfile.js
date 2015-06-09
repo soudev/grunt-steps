dd262026ad76604b28ca1e057439e677fcb03664
Erko Bridee <erko.bridee@gmail.com>
2015-06-09 17:32:56 -0300

    grunt step 01 - Gruntfile.js plugins load, clean, copy and bower tasks defined

--- 01/Gruntfile.js
+++ 01/Gruntfile.js
@@ -2,7 +2,18 @@ module.exports = function(grunt) {
   //----------------------------------------------------------------------------
   // @begin: grunt plugins
 
-    // TODO: define
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-lintspaces');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-filerev');
+  grunt.loadNpmTasks('grunt-usemin');
 
   // @begin: grunt plugins
   //----------------------------------------------------------------------------
@@ -10,7 +21,29 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      dist: ['dist'],
+      bower: ['.local/bower']
+    }, // @end: clean
+
+    copy: {
+      bower_jquery: {
+        expand: true,
+        cwd: 'bower_components/jquery/dist/',
+        src: '**',
+        dest: '.local/bower/vendor/jquery/'
+      },
+
+      bower_dist: {
+        expand: true,
+        cwd: '.local/bower/',
+        src: '**',
+        dest: 'dist/'
+      }
+    }, // @end: copy
+
 
     // TODO: review and define grunt tasks config
 
@@ -21,11 +54,48 @@ module.exports = function(grunt) {
   // @begin: grunt tasks =======================================================
   //============================================================================
 
- // Default task(s).
-  grunt.registerTask('default', 'Grunt.js default task', function() {
-    grunt.log.write('Hello World... ').ok();
+  grunt.registerTask('projectInfoMsg', function() {
+    var pkg = grunt.config.get('pkg');
+
+    grunt.log.writeln('');
+    grunt.log.writeln('project: ' + pkg.name + ' v' + pkg.version);
+    grunt.log.writeln('description: ' + pkg.description);
   });
 
+  //----------------------------------------------------------------------------
+  // @begin: bower
+
+  // 'bower:dev'
+  grunt.registerTask('bower:dev', ['clean:bower', 'copy:bower_jquery']);
+
+  grunt.registerTask('bower:dist', ['bower:dev', 'copy:bower_dist']);
+
+  // @end: bower
+  //----------------------------------------------------------------------------
+
+  // TODO: define tasks
+
+  //----------------------------------------------------------------------------
+  // @begin: main
+  grunt.registerTask('default', function() {
+    grunt.task.run('projectInfoMsg');
+
+    grunt.log.writeln('TODO: define development workflow');
+  });
+
+  grunt.registerTask('release', function() {
+    grunt.task.run('projectInfoMsg');
+
+    grunt.log.writeln('TODO: define build workflow');
+  });
+
+  grunt.registerTask('preview', function() {
+    grunt.task.run('projectInfoMsg');
+
+    grunt.log.writeln('TODO: define preview workflow');
+  });
+
+  // @end: main
   //============================================================================
   // @end: grunt tasks =========================================================
   //============================================================================
