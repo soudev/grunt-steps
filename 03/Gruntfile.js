af95f87d61fdcff47c3e96dde19d4d7ab8d6dfdd
Erko Bridee <erko.bridee@gmail.com>
2015-06-10 12:07:36 -0300

    grunt step 03 - grunt browserSync task config moved to grunt/config_task/browserSync.js

--- 03/Gruntfile.js
+++ 03/Gruntfile.js
@@ -20,33 +20,6 @@ module.exports = function(grunt) {
     config: require('./grunt/config'),
 
 
-    browserSync: {
-      dev: {
-        bsFiles: {
-          src: [
-            '<%= config.paths.build %>/**/*.{html,css,js}'
-          ]
-        },
-        options: {
-          port: '<%= config.webserver.port %>',
-          server: {
-            baseDir: ['<%= config.paths.build %>', '<%= config.paths.bower.toUse %>']
-          },
-          watchTask: true
-        }
-      },
-
-      dist: {
-        options: {
-          ui: false,
-          port: '<%= config.webserver.port %>',
-          server: {
-            baseDir: ['<%= config.paths.dist %>']
-          }
-        }
-      }
-    }, // @end: browserSync
-
     watch: {
       project_html: {
         files: '<%= config.project.html %>',
